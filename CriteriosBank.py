e7bba5d89d87d42649350a72e1c16d56ca228f48
GabrielBueno200 <56837996+GabrielBueno200@users.noreply.github.com>
2022-03-09 23:15:34 -0300

    added all files

---
+++ CriteriosBank.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+from sklearn import tree, metrics
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+from scipy.io import arff
+
+def criteriosBank():
+    data, meta = arff.loadarff('./bank.arff')
+
+    attributes = ['age','average', 'day','duration','campaign','pdays','previous']
+
+    attributesValues = (np.asarray(data[attr]).reshape(-1, 1) for attr in attributes)
+
+    features = np.concatenate(attributesValues, axis=1)
+    target = data['subscribed']
+
+    Arvore = DecisionTreeClassifier(criterion='entropy').fit(features, target)
+
+    plt.figure(figsize=(10, 6.5))
+    tree.plot_tree(Arvore, feature_names=attributes, class_names=['yes', 'no'], filled=True, rounded=True)
+    plt.show()
+
+    fig, ax = plt.subplots(figsize=(25, 10))
+    metrics.plot_confusion_matrix(Arvore, features, target, display_labels=['yes', 'no'], values_format='d',ax=ax)
+    plt.show()
---
+++ CriteriosProvas.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+from sklearn import tree, metrics
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+from scipy.io import arff
+
+def criterioProvas():
+    data,meta = arff.loadarff('./CriterioProvas.arff')
+
+    percFalta = np.asarray(data['PercFalta']).reshape(-1,1)
+    notas_p1 = np.asarray(data['P1']).reshape(-1,1)
+    notas_p2 = np.asarray(data['P2']).reshape(-1,1)
+    features = np.concatenate((notas_p1, notas_p2 , percFalta),axis=1)
+    target = data['resultado']
+
+
+    Arvore = DecisionTreeClassifier(criterion='entropy').fit(features, target)
+
+    plt.figure(figsize=(10, 6.5))
+    tree.plot_tree(Arvore,feature_names=['P1','P2','PercFalta'],class_names=['Aprovado', 'Reprovado'],
+                   filled=True, rounded=True)
+    plt.show()
+
+    fig, ax = plt.subplots(figsize=(25, 10))
+    metrics.plot_confusion_matrix(Arvore,features,target,display_labels=['Aprovado', 'Reprovado'], values_format='d', ax=ax)
+    plt.show()
+
+criterioProvas()
