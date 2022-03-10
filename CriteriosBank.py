ac1eb19bf375b0cad9d852ceb3c3dfb790db90ec
GabrielBueno200 <56837996+GabrielBueno200@users.noreply.github.com>
2022-03-10 00:51:26 -0300

    fixed CriteriosBank

--- CriteriosBank.py
+++ CriteriosBank.py
@@ -5,22 +5,27 @@ from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 from scipy.io import arff
 
-def criteriosBank():
+def bankCriteria():
     data, meta = arff.loadarff('./bank.arff')
 
+    # region Defining the tree
     attributes = ['age','average', 'day','duration','campaign','pdays','previous']
-
-    attributesValues = (np.asarray(data[attr]).reshape(-1, 1) for attr in attributes)
+    attributesValues = [np.asarray(data[attribute]).reshape(-1, 1) for attribute in attributes]
 
     features = np.concatenate(attributesValues, axis=1)
     target = data['subscribed']
+    # endregion
 
-    Arvore = DecisionTreeClassifier(criterion='entropy').fit(features, target)
+    decisionTree = DecisionTreeClassifier(criterion='entropy').fit(features, target)
 
+    # region Plot
     plt.figure(figsize=(10, 6.5))
-    tree.plot_tree(Arvore, feature_names=attributes, class_names=['yes', 'no'], filled=True, rounded=True)
+    tree.plot_tree(decisionTree, feature_names=attributes, class_names=['yes', 'no'], filled=True, rounded=True)
     plt.show()
 
     fig, ax = plt.subplots(figsize=(25, 10))
-    metrics.plot_confusion_matrix(Arvore, features, target, display_labels=['yes', 'no'], values_format='d',ax=ax)
+    metrics.plot_confusion_matrix(decisionTree, features, target, display_labels=['yes', 'no'], values_format='d',ax=ax)
     plt.show()
+    # endregion
+
+bankCriteria()
